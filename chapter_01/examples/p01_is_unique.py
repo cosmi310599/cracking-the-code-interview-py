45def4f7d4f7977a3816e03644e99a2bd998227b
Cosmina <cosmi310599@gmail.com>
2023-04-23 21:59:53 +0200

    Add bitwise operators note

--- chapter_01/examples/p01_is_unique.py
+++ chapter_01/examples/p01_is_unique.py
@@ -1,3 +1,5 @@
+# import unittest
+
 """
 Problem statment
 
@@ -8,7 +10,7 @@ What if you cannot use additional data structures?
 # Example 1
 
 def has_unique_chars(string):
-    """
+    """_summary_
     Check if string has more characters than the number 
     of unique characters in ASCII, the reason of this is because in 
     implementation of algorithms to determine if a string has all unique 
@@ -35,24 +37,62 @@ def has_unique_chars(string):
 # Example 2
 
 def has_unique_chars(s):
+    """_summary_
+
+    Args:
+        s (str): The string to check for uniqueness.
+        
+    Returns:
+        bool: True if the string has no repeated characters, False otherwise.
+    
+    This function checks if the input string `s` contains any repeated characters by iterating over
+    all pairs of characters in the string and checking for equality. If a pair of equal characters is
+    found, the function returns False. If no repeated characters are found, the function returns True.
+
+    This algorithm has a time complexity of O(n^2), where n is the length of the input string. 
+    If you have large input strings, this algorithm may not be efficient. 
+
     """
-    Returns True if a string has all unique characters, False otherwise.
-    """
+    
     for i in range(len(s)):
         for j in range(i+1, len(s)):
             if s[i] == s[j]:
                 return False
     return True
 
-"""
-This algorithm works by comparing each character in the string
-with all other characters in the string. If a match is found, 
-the function immediately returns False, indicating that the string 
-has duplicate characters. If no duplicates are found after comparing all 
-pairs of characters, the function returns True.
 
-This algorithm has a time complexity of O(n^2), where n is the length of the input string. 
-If you have large input strings, this algorithm may not be efficient. In that case, you may want to consider
-using a different algorithm or adding additional data structures to improve the time complexity.
-"""
 
+# Example 3
+
+def is_unique_bit_vector(string):
+    """
+    The function first checks if the length of the string is greater than 120, which is the max number of unique characters that can be
+    represented using 7 bits (since ASCII characters are 7 bits wide), if string length is greater than 128 returns False.
+
+    Initializes the `checker` variable to 0, which will be used to keep track of the characters that have been seen so far.
+    Then iterates over each character in the string and performs the following:
+        - By ord method the ascii value of the character is obtained
+        - The function checks if the 'val' bit is already set in the cheacker variable by performing a bitwise AND operator between 
+          checket and 1 << val, if the result of the biwise is greater than 0 means that the val is already in cheacker
+        - If val bit is not already set in cheacker, the function sets the val bit in cheacker by performing a bitwise OR operator between cheacker
+          and 1 << val
+    """
+    if len(string) > 128:
+        return False
+
+    checker = 0
+    for c in string:
+        val = ord(c)
+        if (checker & (1 << val)) > 0:
+            return False
+        checker |= 1 << val
+    return True
+
+
+
+# TODO: UNIT TEST CLASS
+
+# class TestHasUniqueChars(unittest.TestCase):
+
+# if __name__ == '__main__':
+    # unittest.main()
